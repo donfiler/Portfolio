0bc8ca625a8cd451a1ecbf2422e43aab87ad6724
Don Filer <dfiler@rocketmail.com>
2019-02-08 09:09:21 -0800

    Menu Overlay and several corrections

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,9 @@
 // Select DOM Items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('..menu-nav');
+const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('nav-item');
+const navItems = document.querySelectorAll('.nav-item');
 
 // Set Initial State Of Menu
 let showMenu = false;
